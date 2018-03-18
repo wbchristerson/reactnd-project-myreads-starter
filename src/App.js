bd876557afb6854b95d8dfb9854b017e5211190c
Will Christerson <wbchristerson@gmail.com>
2018-03-17 21:20:22 -0400

    refactor: Remove onclick events

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import React from 'react'
 import './App.css'
 
 class Book extends React.Component {
+  // state = {
+  //   shelf: ''
+  // }
+  //
+  // componentWillMount() {
+  //   this.setState({
+  //     shelf: this.props.bookShelf
+  //   })
+  // }
+  //
+  // repositionBook = (book, shelf) => {
+  //   book.setState({
+  //     shelf: shelf
+  //   })
+  // }
+
+  // On change:
+  // onChange={(event) => {
+  //     console.log(event.target.value)
+  //     this.props.repositionBook(event.target.value)
+  //     console.log("Shelf: ", state.shelf)
+  //   }}
+
   render() {
     return (
       <li>
@@ -34,8 +57,11 @@ class Shelf extends React.Component {
         <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {this.props.books.map((book) => (
-              <Book key={book.title} bookTitle={book.title} bookAuthor={book.author} coverURL={book.url}/>
+            {this.props.books
+              .filter(book => book.status === this.props.category)
+              .map((book) => (
+                <Book key={book.title} bookShelf={this.props.shelfTitle}
+                 bookTitle={book.title} bookAuthor={book.author} coverURL={book.url} repositionBooks={this.repositionBooks}/>
             ))}
           </ol>
         </div>
@@ -54,48 +80,66 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
-    currentlyReading: [
+    heldBooks: [
       {
+        status: "currentlyReading",
         title: "To Kill A Mockingbird",
         author: "Harper Lee",
         url: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
       },
       {
+        status: "currentlyReading",
         title: "Ender's Game",
         author: "Orson Scott Card",
         url: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-      }
-    ],
-    wantToRead: [
+      },
       {
+        status: "wantToRead",
         title: "1776",
         author: "David McCullough",
         url: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
       },
       {
+        status: "wantToRead",
         title: "Harry Potter and the Sorcerer's Stone",
         author: "J.K. Rowling",
         url: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-      }
-    ],
-    read: [
+      },
       {
+        status: "read",
         title: "The Hobbit",
         author: "J.R.R. Tolkien",
         url: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
       },
       {
+        status: "read",
         title: "Oh, the Places You'll Go!",
         author: "Seuss",
         url: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
       },
       {
+        status: "read",
         title: "The Adventures of Tom Sawyer",
         author: "Mark Twain",
         url: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"
       }
     ]
   }
+
+  // moveBook = (shelf) => {
+  //   this.setState((prevState) => ({
+  //     heldBooks: prevState.heldBooks.map((newBook) => {
+  //       if (newBook.title === title) {
+  //         let entry = newBook
+  //         entry.status = shelf
+  //         return entry
+  //       }
+  //       else {
+  //         return newBook
+  //       }
+  //     })
+  //   }))
+  // }
 
   render() {
     return (
@@ -128,9 +172,9 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <Shelf shelfTitle="Currently Reading" books={this.state.currentlyReading}/>
-                <Shelf shelfTitle="Want to Read" books={this.state.wantToRead}/>
-                <Shelf shelfTitle="Read" books={this.state.read}/>
+                <Shelf shelfTitle="Currently Reading" category="currentlyReading" books={this.state.heldBooks}/>
+                <Shelf shelfTitle="Want to Read" category="wantToRead" books={this.state.heldBooks}/>
+                <Shelf shelfTitle="Read" category="read" books={this.state.heldBooks}/>
               </div>
             </div>
             <div className="open-search">
