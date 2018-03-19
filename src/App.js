96dc6541240dadafb816d6373d5bf8f50111998d
Will Christerson <wbchristerson@gmail.com>
2018-03-18 22:05:07 -0400

    refactor: Re-organize components

--- src/App.js
+++ src/App.js
@@ -1,90 +1,8 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
+import Shelves from './Shelves'
+import Search from './Search'
 import './App.css'
-
-class Book extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {value: {props.bookShelf}};
-  // }
-  state = {
-    value: this.props.bookShelf
-  }
-
-  //
-  // componentWillMount() {
-  //   this.setState({
-  //     shelf: this.props.bookShelf
-  //   })
-  // }
-  //
-  // repositionBook = (book, shelf) => {
-  //   book.setState({
-  //     shelf: shelf
-  //   })
-  // }
-
-  // On change:
-  // onChange={(event) => {
-  //     console.log(event.target.value)
-  //     this.props.repositionBook(event.target.value)
-  //     console.log("Shelf: ", state.shelf)
-  //   }}
-
-  // shelfUpdate = (event) => {
-  //
-  //   this.props.shelfChange(this.props.bookTitle, event.target.value)
-  // }
-
-  handleChange(event) {
-    this.setState({value: event.target.value})
-    this.props.shelfChange(this.props.bookTitle, event.target.value)
-  }
-
-  render() {
-    return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.coverURL})` }}></div>
-            <div className="book-shelf-changer">
-              <select value={this.state.value} onChange={(event) => this.handleChange(event)}>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-          <div className="book-title">{this.props.bookTitle}</div>
-          <div className="book-authors">{this.props.bookAuthor}</div>
-        </div>
-      </li>
-    )
-  }
-}
-
-class Shelf extends React.Component {
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {this.props.books
-              .filter(book => book.status === this.props.category)
-              .map((book) => (
-                <Book key={book.title} bookShelf={this.props.category} shelfChange={this.props.shelfChange}
-                 bookTitle={book.title} bookAuthor={book.author} coverURL={book.url} repositionBooks={this.repositionBooks}/>
-            ))}
-          </ol>
-        </div>
-      </div>
-    )
-  }
-}
-
 
 class BooksApp extends React.Component {
   state = {
@@ -95,121 +13,27 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
-    heldBooks: [
-      {
-        status: "currentlyReading",
-        title: "To Kill A Mockingbird",
-        author: "Harper Lee",
-        url: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
-      },
-      {
-        status: "currentlyReading",
-        title: "Ender's Game",
-        author: "Orson Scott Card",
-        url: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-      },
-      {
-        status: "wantToRead",
-        title: "1776",
-        author: "David McCullough",
-        url: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-      },
-      {
-        status: "wantToRead",
-        title: "Harry Potter and the Sorcerer's Stone",
-        author: "J.K. Rowling",
-        url: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-      },
-      {
-        status: "read",
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        url: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
-      },
-      {
-        status: "read",
-        title: "Oh, the Places You'll Go!",
-        author: "Seuss",
-        url: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
-      },
-      {
-        status: "read",
-        title: "The Adventures of Tom Sawyer",
-        author: "Mark Twain",
-        url: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"
-      }
-    ]
   }
 
-  // moveBook = (shelf) => {
-  //   this.setState((prevState) => ({
-  //     heldBooks: prevState.heldBooks.map((newBook) => {
-  //       if (newBook.title === title) {
-  //         let entry = newBook
-  //         entry.status = shelf
-  //         return entry
-  //       }
-  //       else {
-  //         return newBook
-  //       }
-  //     })
-  //   }))
-  // }
-  moveBook = (title, newShelf) => {
-    this.setState((prevState) => ({
-      heldBooks: prevState.heldBooks.map((newBook) => {
-        if (newBook.title === title) {
-          let entry = newBook
-          entry.status = newShelf
-          return entry
-        }
-        return newBook
-      })
-    }))
+  toSearchPage(obj) {
+    obj.setState(
+      { showSearchPage: true }
+    )
+  }
+
+  toShelvesPage(obj) {
+    obj.setState(
+      { showSearchPage: false }
+    )
   }
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <Search setButton={() => this.toShelvesPage(this)}/>
         ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Shelf shelfTitle="Currently Reading" category="currentlyReading" books={this.state.heldBooks}
-                 shelfChange={this.moveBook}/>
-                <Shelf shelfTitle="Want to Read" category="wantToRead" books={this.state.heldBooks}
-                 shelfChange={this.moveBook}/>
-                <Shelf shelfTitle="Read" category="read" books={this.state.heldBooks} shelfChange={this.moveBook}/>
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
+          <Shelves setButton={() => this.toSearchPage(this)}/>
         )}
       </div>
     )
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class Shelf extends Component {
-  state = {
-    books: []
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.sectionTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.bookRow.map(book => (
+                <Book key={book.title} title={book.title} author={book.author} url={book.url}/>
+              )
+            )}
+          </ol>
+        </div>
+      </div>
+    )
   }
 }
+
+export default Shelf
