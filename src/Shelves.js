8b5ecf9472957c31922d868e1fdd13086a7b88c5
Will Christerson <wbchristerson@gmail.com>
2018-03-21 06:34:38 -0400

    style: Re-format Shelves as stateless functional component

--- src/Shelves.js
+++ src/Shelves.js
@@ -2,43 +2,41 @@ import React from 'react'
 import Shelf from './Shelf'
 import { Link } from 'react-router-dom'
 
-class Shelves extends React.Component {
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <Shelf
-              sectionTitle="Currently Reading"
-              shelfType="currentlyReading"
-              bookData={this.props.bookData}
-              updateBookData={this.props.updateBookData}
-            />
-            <Shelf
-              sectionTitle="Want to Read"
-              shelfType="wantToRead"
-              bookData={this.props.bookData}
-              updateBookData={this.props.updateBookData}
-            />
-            <Shelf
-              sectionTitle="Read"
-              shelfType="read"
-              bookData={this.props.bookData}
-              updateBookData={this.props.updateBookData}
-            />
-          </div>
-        </div>
-        <div className="open-search">
-          <Link
-            to="/search"
-          >Add a book</Link>
+function Shelves(props) {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <Shelf
+            sectionTitle="Currently Reading"
+            shelfType="currentlyReading"
+            bookData={props.bookData}
+            updateBookData={props.updateBookData}
+          />
+          <Shelf
+            sectionTitle="Want to Read"
+            shelfType="wantToRead"
+            bookData={props.bookData}
+            updateBookData={props.updateBookData}
+          />
+          <Shelf
+            sectionTitle="Read"
+            shelfType="read"
+            bookData={props.bookData}
+            updateBookData={props.updateBookData}
+          />
         </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <Link
+          to="/search"
+        >Add a book</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Shelves
